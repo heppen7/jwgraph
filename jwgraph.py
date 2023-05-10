ac3744d5f54aac8e9c306c36937561d5ff354ede
heppen <32433090+heppen7@users.noreply.github.com>
2023-05-10 21:52:20 +0200

    add option to set country and lang

--- jwgraph.py
+++ jwgraph.py
@@ -14,20 +14,21 @@ from queries import NEW_TITLE_BUCKETS
 
 class JustWatchAPI:
 
-    def __init__(self):
+    def __init__(self, country: str = '', language: str = '') -> None:
         self.base_url: str = 'https://apis.justwatch.com/graphql'
         self.transport = RequestsHTTPTransport(
             url=self.base_url,
             verify=True,
             retries=3,
         )
-
         self.client = Client(transport=self.transport, fetch_schema_from_transport=False)
+        self.country: str = country if country else 'US'
+        self.language: str = language if language else 'en'
 
     def search_item(self, title: str):
         params = {
-            "country": "US",
-            "language": "en",
+            "country": self.country,
+            "language": self.language,
             "first": 4,
             "filter": {
                 "searchQuery": title
@@ -40,8 +41,8 @@ class JustWatchAPI:
         params = {
             "platform": "WEB",
             "nodeId": node_id,
-            "country": "US",
-            "language": "en",
+            "country": self.country,
+            "language": self.language,
             "filterBuy": {
                 "monetizationTypes": [
                     "BUY"
@@ -78,20 +79,20 @@ class JustWatchAPI:
         params = {
             "platform": "WEB",
             "fullPath": full_path,
-            "language": "en",
-            "country": "US",
+            "language": self.language,
+            "country": self.country,
             "episodeMaxLimit": 20
         }
 
         return self.client.execute(URL_TITLE_DETAILS, variable_values=params)
 
-    def new_titles(self, after=None):
+    def new_titles(self, after: str = ''):
         params = {
                 "allowSponsoredRecommendations": {
                   "country": "PL",
                   "platform": "ANDROID"
                 },
-                "country": "PL",
+                "country": self.country,
                 "filter": {
                   "excludeIrrelevantTitles": False,
                   "objectTypes": [
@@ -101,13 +102,13 @@ class JustWatchAPI:
                 },
                 "first": 5,
                 "imageFormat": "WEBP",
-                "language": "en-US",
+                "language": self.language,
                 "packages": [],
                 "pageType": "NEW",
                 "platform": "ANDROID",
                 "priceDrops": False
               }
-        if after:
+        if after != '':
             params['after'] = after
 
         return self.client.execute(NEW_TITLE_BUCKETS, variable_values=params)
